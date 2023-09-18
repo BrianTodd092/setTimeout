b3106e90cce0848be81f012030ff83305c014058
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2023-09-18 10:06:48 +0100

    Add files via upload
    setTimeout() javaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+const xtime = setTimeout(xheading, 60000);
+const xxtime = setTimeout(xxheading, 60000);
+
+function xheading (){
+
+
+var el =  document.getElementById('img1').style.display="none";
+
+}
+
+
+function xxheading() {
+
+var ela = document.getElementById('img2').style.display="block";
+
+
+
+
+}
