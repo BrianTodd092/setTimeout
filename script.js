32112fcd08820ad01fc60ebea9b61b829e950cc7
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2023-11-25 17:07:04 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-const xtime = setTimeout(xheading, 60000);
-const xxtime = setTimeout(xxheading, 60000);
+const xtime = setTimeout(xheading, 20000);    //20 second.
 
 function xheading (){
 
 
 var el =  document.getElementById('img1').style.display="none";
-
-}
-
-
-function xxheading() {
-
 var ela = document.getElementById('img2').style.display="block";
-
-
-
 
 }
